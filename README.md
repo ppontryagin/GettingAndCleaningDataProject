15ba3332a100a2ff646d9f44cac00ee451bcb742
Pavel.Pontryagin <Pavel.Pontryagin@billing.ru>
2014-12-27 10:36:16 +0300

    Version 2.
    All done. Data is tidy
    No code book

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-Getting and Cleaning Data
+Getting and Cleaning Data: Course Project
 =======================
 
 ## Purpose
 The purpose of this project is to demonstrate my ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. 
-It consists of: 
-
-## Source data
-A full description is available at the site where the data was obtained: 
-http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
-
-Here are the data for the project: 
-https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
-
 
 ## Contents
 - this **README.md**
@@ -22,3 +13,5 @@ https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Datas
 	- Uses descriptive activity names to name the activities in the data set
 	- Appropriately labels the data set with descriptive variable names. 
 	- From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+	
+	
--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,100 @@
-x <- 1
+# 1. Download and unzip files
+
+setwd("c://tmp/r")
+
+if(!file.exists("./data")) {dir.create("./data")}
+
+zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+destFile <- "./data/Dataset.zip"
+download.file(zipUrl, destFile, method="curl")
+
+unzip(destFile, files = NULL, list = FALSE, overwrite = TRUE,  junkpaths = FALSE, exdir = ".", unzip = "internal", setTimes = FALSE)
+
+
+# 2 Initialize data arrays 
+
+# Measurements
+
+## Train
+xTrainFile <- "UCI HAR Dataset/train/X_train.txt"
+xTrainData <- read.table(xTrainFile, sep="")
+
+## Test
+xTestFile <- "UCI HAR Dataset/test/X_test.txt"
+xTestData <- read.table(xTestFile, sep="")
+
+#Activity type
+
+## Train
+yTrainFile <- "UCI HAR Dataset/train/y_train.txt"
+yTrainData <- read.table(yTrainFile, sep="\t")
+
+## Test
+yTestFile <- "UCI HAR Dataset/test/y_test.txt"
+yTestData <- read.table(yTestFile, sep="\t")
+
+#Person id
+## Train
+subjectTrainFile <- "UCI HAR Dataset/train/subject_train.txt"
+subjectTrainData <- read.table(subjectTrainFile, sep="\t")
+
+## Test
+subjectTestFile <- "UCI HAR Dataset/test/subject_test.txt"
+subjectTestData <- read.table(subjectTestFile, sep="\t")
+
+#Column names
+featuresFile <- "UCI HAR Dataset/features.txt"
+featuresData <- read.table(featuresFile, sep=" ")
+
+#Activity labels
+actLabelFile <- "UCI HAR Dataset/activity_labels.txt"
+actLabels <- read.table(actLabelFile, sep="")
+
+
+# 3.  Label and combine data
+## Train data
+# Combine datasets
+trainDenorm <- cbind(subjectTrainData, yTrainData, xTrainData, deparse.level = 1)
+
+# Add column names
+header <- c("subject", "activity", as.character(featuresData$V2))
+colnames(trainDenorm) <- header
+
+# substitue activity type
+trainDenorm$activity <- actLabels$V2[match(trainDenorm$activity, actLabels$V1)]
+
+#Grep std, mean
+trainDenorm <- trainDenorm[grep("subject|activity|mean|std", names(trainDenorm))]
+head(trainDenorm)
+
+## Test data
+# Combine datasets
+testDenorm <- cbind(subjectTestData, yTestData, xTestData, deparse.level = 1)
+
+# Add column names
+### 4. Appropriately labels the data set with descriptive variable names. 
+header <- c("subject", "activity", as.character(featuresData$V2))
+colnames(testDenorm) <- header
+
+# substitue activity type
+### 3. Uses descriptive activity names to name the activities in the data set
+testDenorm$activity <- actLabels$V2[match(testDenorm$activity, actLabels$V1)]
+
+#Grep std, mean
+### 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+testDenorm <- testDenorm[grep("subject|activity|mean|std", names(testDenorm))]
+head(testDenorm)
+
+# 4 Combine train and test datasets
+### 1. Merge the training and the test sets to create one data set.
+tidyData <- rbind(testDenorm, trainDenorm)
+
+#======================
+### From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+colNames2Aggregate <- grep("mean|std", names(tidyData))
+aggrData <- aggregate(tidyData[, colNames2Aggregate], list(subject=tidyData$subject, activity=tidyData$activity), mean)
+
+write.table(aggrData, "aggr_data.txt", row.name=FALSE)
+
+
